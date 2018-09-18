815347cf18cf5244578851b8f5d15d4cbe2a8bb7
vitalwarley <wvb@ic.ufal.br>
2018-09-18 07:38:28 -0300

    Add android base project

---
+++ app/src/main/java/cv/ic/ufal/edu/br/cvproj/MainActivity.java
@@ -0,0 +1,13 @@
+package cv.ic.ufal.edu.br.cvproj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
