2750aea28d06a29af679796bfe25022dd8b924a0
vitalwarley <wvb@ic.ufal.br>
2018-09-18 08:20:33 -0300

    Add CvCameraViewListener2 interface
    With this contract we must implement the necessary methods to interact
    with the camera. They are:
    
    - `onCameraViewStarted`
    - `onCameraViewStopped`
    - `onCameraFrame`
    
    Check out #2 to more info about them.

--- app/src/main/java/cv/ic/ufal/edu/br/cvproj/MainActivity.java
+++ app/src/main/java/cv/ic/ufal/edu/br/cvproj/MainActivity.java
@@ -3,11 +3,30 @@ package cv.ic.ufal.edu.br.cvproj;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
     }
 }
