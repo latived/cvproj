8795de32fb6f0589d7648f1640b1ac012a964903
vitalwarley <wvb@ic.ufal.br>
2018-09-26 22:58:17 -0300

    Fix previous commit (didn't add MainActivity); add onManagerConnected implementation

--- app/src/main/java/cv/ic/ufal/edu/br/cvproj/MainActivity.java
+++ app/src/main/java/cv/ic/ufal/edu/br/cvproj/MainActivity.java
@@ -1,5 +1,6 @@
 package cv.ic.ufal.edu.br.cvproj;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,9 @@ import org.opencv.core.Scalar;
 import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
 
@@ -42,6 +46,34 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
+
+                    // Load native lib before OpenCV init (libname is equal as in Android.mk)
+                    System.loadLibrary("detection_based_tracker");
+
+                    try {
+
+                        InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        os.close();
+
+                        mNativeDetector = new DetectionBasedTracker(mCascadeFile.getAbsolutePath(), 0);
+
+                        cascadeDir.delete();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+                    }
+
                     mOpenCvCameraView.enableView();
                 } break;
                 default: {
